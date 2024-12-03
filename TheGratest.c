4bb4908ec3e093a16510acae43fcfaff0dfbfd20
isabeltsukayama <isabel.tsukayama@unifesp.br>
2024-12-03 19:29:28 -0300

    Create TheGratest.c

---
+++ TheGratest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, C, maior;
+    
+    scanf("%d %d %d", &A, &B, &C);
+    
+    maior = (A + B + abs(A - B)) / 2;
+    maior = (maior + C + abs(maior - C)) / 2;
+    
+    printf("%d eh o maior\n", maior);
+    
+    return 0;
+}
